23774e3d4a4a79a39c64561f06002cf0e9431a56
kiborgok <borgokalek14@gmail.com>
2018-12-24 12:27:01 +0300

    first commit

--- setup.py
+++ setup.py
@@ -10,3 +10,7 @@ def index():
 @app.route('/hello/<username>')
 def hello(username=None):
     return render_template('hello.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return '<h2>Login</h2>'
